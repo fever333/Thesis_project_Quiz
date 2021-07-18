da8c37386159309147d23480d0cbad4998c83a99
fever333 <philadeathcore2@mail.ru>
2021-07-18 08:42:19 +0300

    thesis project

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <div style={{textAlign:'center',marginBottom:10}}>Quiz made by {''}
+        <a 
+         href="https://github.com/fever333"
+         style={{cursor:'pointer',color:'blue'}}
+        >
+        fever333
+        </a>
+        </div>
+    );
+};
+
+export default Footer
